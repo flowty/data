f91088c191e78b156ffbd339a1c055063d1a9d00
Bjørn Petersen <bjorn@flowty.ai>
2024-04-23 11:20:06 +0200

    Added FCMCF

--- src/flowty_data/__main__.py
+++ src/flowty_data/__main__.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 
+from flowty_data.fcmcf import convert_canad
 from flowty_data.mcf import convert_planargrid
 from flowty_data.rcmcf import convert_linerlib
 from flowty_data.vrptw import convert_solomonhg
@@ -9,9 +10,10 @@ from flowty_data.vrptw import convert_solomonhg
 def main():
     argv = sys.argv[1:]
     argv.append("--type")
+    argv.append("fcmcf")
     # argv.append("mcf")
     # argv.append("rcmcf")
-    argv.append("vrptw")
+    # argv.append("vrptw")
 
     parser = argparse.ArgumentParser(
         description="Converts instance data to Flowty format"
@@ -19,6 +21,8 @@ def main():
     parser.add_argument("--type", help="Converts mcf data to Flowty format")
     args = parser.parse_args(argv)
 
+    if args.type == "fcmcf":
+        return convert_canad.convert()
     if args.type == "mcf":
         return convert_planargrid.convert()
     if args.type == "rcmcf":
